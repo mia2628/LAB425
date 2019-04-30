d290cb2ec8975cb67c74485dde1283c7b559d17b
mia2628 <50148752+mia2628@users.noreply.github.com>
2019-05-01 02:14:13 +0900

    Winform GoogleMap
    Winform으로 구글검색하여 위치검색

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0501Google
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_data_Click(object sender, EventArgs e)
+        {
+            string street = txt_street.Text;
+            string city = txt_city.Text;
+            string state = txt_state.Text;
+            string zip = txt_zip.Text;
+            try
+            {
+                StringBuilder queryaddress = new StringBuilder();
+                queryaddress.Append("http://maps.google.com/maps?q=");
+
+                if(street!=string.Empty)
+                {
+                    queryaddress.Append(street+","+"+");
+                }
+                if (city != string.Empty)
+                {
+                    queryaddress.Append(city + "," + "+");
+                }
+                if (state != string.Empty)
+                {
+                    queryaddress.Append(state + "," + "+");
+                }
+                if (zip != string.Empty)
+                {
+                    queryaddress.Append(zip + "," + "+");
+                }
+
+                webBrowser1.Navigate(queryaddress.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+    }
+}
